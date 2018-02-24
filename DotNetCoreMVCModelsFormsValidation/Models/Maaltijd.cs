b95d1eb3ba24c43440f8fcd25970b41d31bc1c49
Olivier Sourie <Olivier.Sourie@howest.be>
2018-02-24 19:03:19 +0100

    Model Maaltijd

---
+++ DotNetCoreMVCModelsFormsValidation/Models/Maaltijd.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNetCoreMVCModelsFormsValidation.Models
+{
+    public enum MaaltijdType
+    {
+        Ontbijt,
+        Lunch,
+        Avondmaal
+    }
+    public class Maaltijd
+    {
+        public string Naam { get; set; }
+        public MaaltijdType Type { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
