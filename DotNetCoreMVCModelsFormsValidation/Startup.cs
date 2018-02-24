4fdcda2ad06130e71a8bcea29f2aacfcce9c3864
Olivier Sourie <Olivier.Sourie@howest.be>
2018-02-24 18:19:10 +0100

    Startup

--- DotNetCoreMVCModelsFormsValidation/Startup.cs
+++ DotNetCoreMVCModelsFormsValidation/Startup.cs
@@ -39,12 +39,13 @@ namespace DotNetCoreMVCModelsFormsValidation
 
             app.UseStaticFiles();
 
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+            app.UseMvcWithDefaultRoute();
+            //app.UseMvc(routes =>
+            //{
+            //    routes.MapRoute(
+            //        name: "default",
+            //        template: "{controller=Home}/{action=Index}/{id?}");
+            //});
         }
     }
 }
